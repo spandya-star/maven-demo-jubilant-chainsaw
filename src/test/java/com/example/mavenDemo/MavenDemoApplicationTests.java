abc8f9bba2c27be70a0beaa733b2249933efba8e
Arpan Vaidya <arpan.vaidya.tech@gmail.com>
2021-01-26 13:39:31 -0500

    maven demo project

---
+++ src/test/java/com/example/mavenDemo/MavenDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mavenDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
